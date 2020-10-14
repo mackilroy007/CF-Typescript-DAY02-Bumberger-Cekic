f142703cab682e1c6833c0130092074c85d0da27
mackilroy007 <mackilroy007@gmail.com>
2020-10-14 10:48:39 +0200

    basic 1 and 2 complete

--- Basic 1/typescript.js
+++ Basic 1/typescript.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //class creation
 var Person = /** @class */ (function () {
     //a, b, c, d represent the ammount of var needed
@@ -12,16 +25,31 @@ var Person = /** @class */ (function () {
         this.jobTitle = d;
     }
     //one function is enough to be called apon when displaying
-    Person.prototype.lidia = function () {
-        return "My name is " + this.firstName + " " + this.lastName + " and I am " + this.age + " \n        years old and I work as a " + this.jobTitle + ".";
+    Person.prototype.lidiar = function () {
+        return "My name is " + this.firstName + " " + this.lastName + " and I am " + this.age + " \n        years old and I work as an " + this.jobTitle + ".";
     };
     //we can chain multiple functions together
     Person.prototype.whoAreYou = function () {
-        return "Hello there! " + this.lidia();
+        return "Hello there! " + this.lidiar();
     };
     return Person;
 }());
 //insert values into the class and display them
 var fullName = new Person("Aaron", "Barbados", "45", "Easy Rider");
-console.log(fullName.lidia());
+console.log(fullName.lidiar());
 //extending the person class with new content
+var Aaron = /** @class */ (function (_super) {
+    __extends(Aaron, _super);
+    function Aaron(a, b, c, d, e, f) {
+        var _this = _super.call(this, a, b, c, d) || this;
+        _this.salary = e;
+        _this.jobLocation = f;
+        return _this;
+    }
+    Aaron.prototype.whoAreYou = function () {
+        return _super.prototype.whoAreYou.call(this) + " My salary is " + this.salary + " every month and I work in a " + this.jobLocation + ".";
+    };
+    return Aaron;
+}(Person));
+var continuedFullName = new Aaron("Aaron", "Barbados", "45", "Easy Rider", "-1$", "basement");
+console.log(continuedFullName.whoAreYou());
--- Basic 1/typescript.ts
+++ Basic 1/typescript.ts
@@ -12,18 +12,32 @@ class Person{
         this.jobTitle = d;
     }
     //one function is enough to be called apon when displaying
-    lidia(){
+    lidiar(){
         return `My name is ${this.firstName} ${this.lastName} and I am ${this.age} 
-        years old and I work as a ${this.jobTitle}.`;
+        years old and I work as an ${this.jobTitle}.`;
     } 
     //we can chain multiple functions together
     whoAreYou(){
-        return `Hello there! ${this.lidia()}`;
+        return `Hello there! ${this.lidiar()}`;
     }
 } 
 
 //insert values into the class and display them
 let fullName = new Person("Aaron", "Barbados", "45", "Easy Rider");
-console.log(fullName.lidia());
+console.log(fullName.lidiar());
 
 //extending the person class with new content
+class Aaron extends Person{
+    salary;
+    jobLocation;
+    constructor(a,b,c,d, e, f){
+        super(a, b, c, d );
+        this.salary = e;
+        this.jobLocation = f;
+    }
+    whoAreYou(){
+        return `${super.whoAreYou()} My salary is ${this.salary} every month and I work in a ${this.jobLocation}.`;
+    }
+}
+let continuedFullName = new Aaron("Aaron", "Barbados", "45", "Easy Rider", "-1$", "basement");
+console.log(continuedFullName.whoAreYou());
