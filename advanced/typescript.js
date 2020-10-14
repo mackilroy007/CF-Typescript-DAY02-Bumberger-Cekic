7502f5a5ae9e362042eb69e9126f35b9d55c2e57
Lidia Cekic <lidiacekic9@gmail.com>
2020-10-14 15:37:19 +0200

    advanced update

--- advanced/typescript.js
+++ advanced/typescript.js
@@ -1,14 +1,25 @@
 'use srtict';
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var arr = [];
 var Vehicles = /** @class */ (function () {
-    function Vehicles(a, b, c, d, e, f) {
-        this.vehicle = '';
+    function Vehicles(b, c, d, e, f) {
         this.price = '';
         this.kilometers = '';
         this.production = '';
         this.seats = '';
         this.fuel = '';
-        this.vehicle = a;
         this.price = b;
         this.kilometers = c;
         this.production = d;
@@ -17,16 +28,65 @@ var Vehicles = /** @class */ (function () {
         arr.push(this);
     }
     Vehicles.prototype.attributes = function () {
-        return " " + this.vehicle + " " + this.price + "  " + this.kilometers + " \n       " + this.production + " " + this.seats + " " + this.fuel;
+        return "  " + this.price + "  " + this.kilometers + " \n       " + this.production + " " + this.seats + " " + this.fuel;
     };
     return Vehicles;
 }());
-var fullName = new Vehicles('F200', 'Aud9411', 'BMW5500', 'mnA4kk', 'mnA4kk', 'mnA4kk');
-var fullName1 = new Vehicles('W200', 'Aud8811', 'BMW3000', 'pAas4kk', 'pAas4kk', 'pAas4kk');
-var fullName2 = new Vehicles('X204', 'Aud7411', 'BMW3400', 'gd4kk', 'pAas4kk', 'pAas4kk');
-var fullName3 = new Vehicles('KTt0', 'Aud31', 'BMW5500', 'ge4kk', 'pAas4kk', 'pAas4kk');
-console.table(arr);
-console.log(arr[0].attributes());
+var motorbike = /** @class */ (function (_super) {
+    __extends(motorbike, _super);
+    function motorbike(b, c, d, e, f, g) {
+        var _this = _super.call(this, b, c, d, e, f) || this;
+        _this.vehicle = g;
+        return _this;
+    }
+    motorbike.prototype.attributes = function () {
+        return " " + this.vehicle + " " + this.price + "  " + this.kilometers + " \n       " + this.production + " " + this.seats + " " + this.fuel;
+    };
+    return motorbike;
+}(Vehicles));
+var car = /** @class */ (function (_super) {
+    __extends(car, _super);
+    function car(b, c, d, e, f, g, h) {
+        var _this = _super.call(this, b, c, d, e, f) || this;
+        _this.vehicle = g;
+        _this.color = h;
+        return _this;
+    }
+    car.prototype.attributes = function () {
+        return "  " + this.vehicle + " " + this.color + " " + this.price + "  " + this.kilometers + " \n       " + this.production + " " + this.seats + " " + this.fuel;
+    };
+    return car;
+}(Vehicles));
+var bus = /** @class */ (function (_super) {
+    __extends(bus, _super);
+    function bus(b, c, d, e, f, g, h) {
+        var _this = _super.call(this, b, c, d, e, f) || this;
+        _this.vehicle = g;
+        _this.size = h;
+        return _this;
+    }
+    bus.prototype.attributes = function () {
+        return " " + this.vehicle + " " + this.size + " " + this.price + "  " + this.kilometers + " \n       " + this.production + " " + this.seats + " " + this.fuel;
+    };
+    return bus;
+}(Vehicles));
+var truck = /** @class */ (function (_super) {
+    __extends(truck, _super);
+    function truck(b, c, d, e, f, g, h) {
+        var _this = _super.call(this, b, c, d, e, f) || this;
+        _this.vehicle = g;
+        _this.owner = h;
+        return _this;
+    }
+    truck.prototype.attributes = function () {
+        return "  " + this.vehicle + " " + this.owner + " " + this.price + "  " + this.kilometers + " \n       " + this.production + " " + this.seats + " " + this.fuel;
+    };
+    return truck;
+}(Vehicles));
+var display1 = new motorbike('10', '100', '99', '2', 'petrol', 'motorbike');
+var display2 = new car('10', '100', '99', '2', 'benzin', 'car', 'blue');
+var display3 = new bus('10', '100', '99', '2', 'diesel', 'bus', '0');
+var display4 = new truck('10', '100', '99', '2', 'diesl', 'truck', 'Aaron');
 for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
     var i = arr_1[_i];
     document.getElementById('result').innerHTML += i.attributes() + " <br>";
--- advanced/typescript.ts
+++ advanced/typescript.ts
@@ -2,15 +2,13 @@
 
 let arr = [];
 class Vehicles {
-  vehicle = '';
   price = '';
   kilometers = '';
   production = '';
   seats = '';
   fuel = '';
 
-  constructor(a, b, c, d, e, f) {
-    this.vehicle = a;
+  constructor(b, c, d, e, f) {
     this.price = b;
     this.kilometers = c;
     this.production = d;
@@ -21,49 +19,65 @@ class Vehicles {
   }
 
   attributes() {
+    return `  ${this.price}  ${this.kilometers} 
+       ${this.production} ${this.seats} ${this.fuel}`;
+  }
+}
+class motorbike extends Vehicles {
+  vehicle;
+  constructor(b, c, d, e, f, g) {
+    super(b, c, d, e, f);
+    this.vehicle = g;
+  }
+  attributes() {
     return ` ${this.vehicle} ${this.price}  ${this.kilometers} 
        ${this.production} ${this.seats} ${this.fuel}`;
   }
-  //we can chain multiple functions together
-  // whoAreYou() {
-  //   return `Hello there! ${this.lidiar()}`;
-  // }
 }
+class car extends Vehicles {
+  vehicle;
+  color;
+  constructor(b, c, d, e, f, g, h) {
+    super(b, c, d, e, f);
+    this.vehicle = g;
+    this.color = h;
+  }
+  attributes() {
+    return `  ${this.vehicle} ${this.color} ${this.price}  ${this.kilometers} 
+       ${this.production} ${this.seats} ${this.fuel}`;
+  }
+}
+class bus extends Vehicles {
+  vehicle;
+  size;
+  constructor(b, c, d, e, f, g, h) {
+    super(b, c, d, e, f);
+    this.vehicle = g;
+    this.size = h;
+  }
+  attributes() {
+    return ` ${this.vehicle} ${this.size} ${this.price}  ${this.kilometers} 
+       ${this.production} ${this.seats} ${this.fuel}`;
+  }
+}
+class truck extends Vehicles {
+  vehicle;
+  owner;
+  constructor(b, c, d, e, f, g, h) {
+    super(b, c, d, e, f);
+    this.vehicle = g;
+    this.owner = h;
+  }
+  attributes() {
+    return `  ${this.vehicle} ${this.owner} ${this.price}  ${this.kilometers} 
+       ${this.production} ${this.seats} ${this.fuel}`;
+  }
+}
+let display1 = new motorbike('10', '100', '99', '2', 'petrol', 'motorbike');
+let display2 = new car('10', '100', '99', '2', 'benzin', 'car', 'blue');
+let display3 = new bus('10', '100', '99', '2', 'diesel', 'bus', '0');
+let display4 = new truck('10', '100', '99', '2', 'diesl', 'truck', 'Aaron');
 
-let fullName = new Vehicles(
-  'F200',
-  'Aud9411',
-  'BMW5500',
-  'mnA4kk',
-  'mnA4kk',
-  'mnA4kk',
-);
-let fullName1 = new Vehicles(
-  'W200',
-  'Aud8811',
-  'BMW3000',
-  'pAas4kk',
-  'pAas4kk',
-  'pAas4kk',
-);
-let fullName2 = new Vehicles(
-  'X204',
-  'Aud7411',
-  'BMW3400',
-  'gd4kk',
-  'pAas4kk',
-  'pAas4kk',
-);
-let fullName3 = new Vehicles(
-  'KTt0',
-  'Aud31',
-  'BMW5500',
-  'ge4kk',
-  'pAas4kk',
-  'pAas4kk',
-);
-console.table(arr);
-console.log(arr[0].attributes());
 for (let i of arr) {
   document.getElementById('result').innerHTML += `${i.attributes()} <br>`;
 }
